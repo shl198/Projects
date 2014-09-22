fd237a2208c419ade371640f947dcc9c70af344c
shangzhong <shangzhong@calit2-137-110-116-12.ucsd.edu>
2014-09-22 16:46:09 -0700

    add get parameters function

--- Modules/FileProcess.py
+++ Modules/FileProcess.py
@@ -9,6 +9,29 @@ def remove(files):
         cmd = ''
         for f in files:
             cmd = cmd + 'rm {file} & '.format(file=f)
-        subprocess.call(cmd[:-2],shell=True)
+        subprocess.call(cmd[:-3],shell=True)
     
-        
+
+def get_parameters(parFile):
+    """
+    This function list all parameters for all pipelines.
+    And return a dictionary
+    """
+    res = open(parFile)
+    dic = {}
+    for line in res:
+        if line[0].isalpha():
+            item = line.split('\t')
+            value = item[1] # combine all values into a single string
+            if ',' in value:
+                rg = value.split(',')
+                rg[-1] = rg[-1][:-1]
+                dic[item[0]] = rg
+            else:
+                dic[item[0]] = item[1][:-1]
+        else:
+            continue
+    if isinstance(dic['readGroup'],str):
+                dic['readGroup'] = [dic['readGroup'][:-1]]
+    return dic
+
