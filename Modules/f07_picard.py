d09ccd2502a0a89264853f2802c42a9abe5d0af0
shangzhong <shangzhong@calit2-137-110-116-12.ucsd.edu>
2014-10-08 11:43:07 -0700

    fix some errors in markduplicates

--- Modules/f07_picard.py
+++ Modules/f07_picard.py
@@ -19,22 +19,51 @@ def sam2sortbam(picard,samfiles):
         subprocess.call(cmd,shell=True)
     return sorted_files
 
-def markduplicates(picard,sortbam):
+def markduplicates(picard,sortBams):
     """
     this function mark duplicates of the sorted bam file
     """
     mark = picard + '/' + 'MarkDuplicates.jar'
     dedup_files = []
     cmd = ''
-    for bam in sortbam:
+    for bam in sortBams:
         dedup = bam[:-8] + 'dedup.bam'
         dedup_files.append(dedup)
         cmd = cmd + ('java -jar {mark} I={input} O={output} CREATE_INDEX=true '
-        'METRICS_FILE=metrics.txt MAX_RECORDS_IN_RAM=2000000 '
-        'MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 & ').format(mark=mark,input=bam,
+        'METRICS_FILE=metrics.txt MAX_RECORDS_IN_RAM=8000000 '
+        'MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 '
+        'VALIDATION_STRINGENCY=LENIENT && ').format(mark=mark,input=bam,
         output=dedup)
-    subprocess.call(cmd[:-2],shell=True)
+    subprocess.call(cmd[:-3],shell=True)
     return dedup_files
 
-
-        
+def addReadGroup(picard,sortBamFiles,readgroups):
+    """
+    This function add readgroup to a list of samfiles
+    """
+    add = picard + '/' + 'AddOrReplaceReadGroups.jar'
+    sortBams = []
+    cmd = ''
+    for sam,rg in zip(sortBamFiles,readgroups):
+        sortbam = sam[:-3] + 'sort.bam'
+        sortBams.append(sortbam)
+        readgroup = rg.split('\\t')
+        ID = readgroup[1][3:-1]
+        SM = readgroup[2][3:-1]
+        PL = readgroup[3][3:-1]
+        LB = readgroup[4][3:-1]
+        PU = readgroup[5][3:]
+        cmd = cmd + ('java -jar {addGp} I={input} O={sortbam} '
+                     'RGID={ID} RGSM={SM} RGPL={PL} RGLB={LB} RGPU={PU} & ').format(
+                    addGp=add,input=sam,sortbam=sortbam,ID=ID,SM=SM,PL=PL,LB=LB,
+                    PU=PU)
+    subprocess.call(cmd[:-3],shell=True)
+    # the file name in sortBams is filename.sort.sort.bam, need to change to filename.sort.bam
+    final_sort_bams = []
+    for bam in sortBams:
+        finalSortBam = bam[:-13] + 'sort.bam'
+        final_sort_bams.append(finalSortBam)
+        renameCmd = ('mv {sortBam} {finalSortBam}').format(sortBam=bam,finalSortBam=finalSortBam)
+        subprocess.call(renameCmd,shell=True)
+    
+    return final_sort_bams    
