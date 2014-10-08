c5dfbeb17c234aa6edb3d6e3848ea7f7abf89579
shangzhong <shangzhong@calit2-137-110-116-12.ucsd.edu>
2014-10-08 10:50:44 -0700

    1st complete version of RNAseq pipeline

--- VariantCall/GATK_RNA_vari_call.py
+++ VariantCall/GATK_RNA_vari_call.py
@@ -4,6 +4,7 @@ this file does variant calling for RNAseq
 #=============  import required packages  =================
 import os
 import sys
+import subprocess
 sys.path.append('/home/shangzhong/Codes/Pipeline')
 from Modules.f00_Message import Message
 from Modules.f01_list_trim_fq import list_files,Trimmomatic
@@ -18,8 +19,8 @@ from Modules.FileProcess import remove,get_parameters,rg_bams
    running pipeline
 """
 
-parFile = '/home/shangzhong/Codes/Pipeline/VariantCall/RNA_Parameters.txt'
-# parFile = sys.argv[1]
+#parFile = '/home/shangzhong/Codes/Pipeline/VariantCall/RNA_Parameters.txt'
+parFile = sys.argv[1]
 param = get_parameters(parFile)
 thread = param['thread']
 email = param['email']
@@ -42,19 +43,80 @@ organism = param['organism']
 #========  1. map and dedupping =====================================
 #========  (0) enter the directory ========================
 os.chdir(file_path)
-#Message(startMessage,email)
+Message(startMessage,email)
 #========  (1) read files  ================================
 fastqFiles = list_files(file_path)
 if trim == 'True':
     fastqFiles = Trimmomatic(fastqFiles,phred)
-print 'list file succeed'
+subprocess.call('echo \"list file succeed\"',shell=True)
 #========  (2) align using 2 pass STAR ====================
 map_sams= STAR2Pass(fastqFiles,starDb,ref_fa,thread)
+subprocess.call('echo \"align succeed\"',shell=True)
 
 #========  2. Add read groups, sort,mark duplicates, and create index
 #========  (1) sort and add group =========================
-sort_bams = addReadGroup(picard,map_sams,read_group)
+sort_bams = sam2bam_sort(map_sams,thread)
+subprocess.call('echo \"sort bam succeed\"',shell=True)
+
+group_bams = addReadGroup(picard,sort_bams,read_group)
+subprocess.call('echo \"add group succeed\"',shell=True)
+remove(map_sams)
 #========  (2) mark duplicates ============================
-dedup_bams = markduplicates(picard,sort_bams)
+dedup_bams = markduplicates(picard,group_bams)
+subprocess.call('echo \"mark duplicate succeed\"',shell=True)
+remove(sort_bams)
 
 #========  3. Split 'N' Trim and reassign mapping qualiteies
+split_bams = splitN(gatk,dedup_bams,ref_fa)
+subprocess.call('echo \"split N succeed\"',shell=True)
+
+#========  4. Indel realignment ===========================
+#========  (1) generate intervals =========================
+interval = RealignerTargetCreator(gatk,split_bams,ref_fa,thread)
+subprocess.call('echo \"RealignerTarget Creator succeed\"',shell=True)
+#========  (2) realignment of target intervals ============
+realign_bams = IndelRealigner(gatk,split_bams,ref_fa,interval)
+subprocess.call('echo \"IndelRealigner succeed\"',shell=True)
+remove(split_bams)
+
+#========  5. Base quality recalibration  =================
+
+# since we don't have dbsnp for CHO, we need to:
+# 1. find snp without recalibration, got vcf file
+# 2. extract the snps we think are real snps, into a real_vcf file.
+# 3. use the file in 2 to do the recalibration.
+
+roundNum = 1
+#========  6. Variant Calling =============================
+vcf_files = HaplotypeCaller_RNA_VCF(gatk,realign_bams,ref_fa)
+subprocess.call('echo \"1 round call succeed\"',shell=True)
+
+#========  7. Variant filtering ===========================
+gold_varis = RNA_Vari_Filter(gatk,vcf_files,ref_fa)
+subprocess.call('echo \"1 round gold variant succeed\"',shell=True)
+
+#========  5. Base quality recalibration  =================
+recal_bams = RNA_BaseRecalibrator(gatk,realign_bams,ref_fa,
+            gold_varis,roundNum,thread)
+subprocess.call('echo \"recalibration succeed\"',shell=True)
+
+#========  !!! merge lanes for the same sample ============
+roundNum = '1'
+if len(recal_bams) !=1:
+    merged_bams = rg_bams(read_group,recal_bams)
+    remove(recal_bams)
+    dedup_files = markduplicates(picard,merged_bams)
+    remove(merged_bams)
+    interval = RealignerTargetCreator(gatk,dedup_files,ref_fa,thread)
+    realign_bams = IndelRealigner(gatk,dedup_files,ref_fa,interval)
+    remove(dedup_files)
+subprocess.call('echo \"merge lanes succeed\"',shell=True)
+
+roundNum = 2
+#========  6. Variant Calling =============================
+vcf_files = HaplotypeCaller_RNA_VCF(gatk,realign_bams,ref_fa)
+subprocess.call('echo \"2 round call succeed\"',shell=True)
+#========  7. Variant filtering ===========================
+gold_varis = RNA_Vari_Filter(gatk,vcf_files,ref_fa)
+subprocess.call('echo \"2 round gold variant succeed\"',shell=True)
+subprocess.call('echo \" job finished succeessfully\"',shell=True)
