5a5db8fcc666b5b7944664936cfd76eb17ce9721
shangzhong <shangzhong@calit2-137-110-116-12.ucsd.edu>
2014-10-16 10:22:18 -0700

    finish pipeline for automatic DE analyse including run in R

--- DiffExpression/DE_pipeline.py
+++ DiffExpression/DE_pipeline.py
@@ -2,41 +2,64 @@
 This file run the pipeline of doing differential expression analysis of 
 Contaminated CHO samples.
 """
-import os 
+import os
+import sys 
 import subprocess
-from Modules.f01_list_files import list_fastq
+sys.path.append('/home/shangzhong/Codes/Pipeline')
+from Modules.f00_Message import Message
+from Modules.f01_list_trim_fq import list_files,Trimmomatic
 from Modules.f02_aligner_command import gsnap,STAR
 from Modules.f03_samtools import sam2bam_sort
 from Modules.f04_htseq import htseq_count
 from Modules.f05_GeneIDConvert import ID_Convert
-#=========== define some pathways ===================
-#file_path = sys.argv[1]
-file_path = '/data/shangzhong/Diff_express/star_test'
-db_path = '/opt/genome/cho/gsnap_chok1Db'
-db_name = 'chok1'
-gsnap_annotation = '/opt/genome/cho/gsnap_chok1Db/chok1.maps/chok1.splicesites.iit'
-thread = '1'
-annotation = '/opt/genome/cho/chok1.gff3'
+from Modules.FileProcess import remove,get_parameters
+#=========== define some parameters ===================
+#parFile = '/home/shangzhong/Codes/Pipeline/DiffExpression/DE_Parameters.txt'
+parFile = sys.argv[1]
+param = get_parameters(parFile)
 
-Dict = '/home/shangzhong/Database/chok1_geneID_symbol.txt'
-output_path = '/data/RNAseq/htseq_count' 
+thread = param['thread']
+email = param['email']
+startMessage = param['startMessage']
+endMessage = param['endMessage']
+ref_fa = param['refSequence']
+file_path = param['filePath']
+db_path = param['alignerDb']
+trim = param['trim']
+phred = param['phred']
+trimmomatic = param['trimmomatic']
+trimmoAdapter = param['trimmoAdapter']
+
+aligner = param['aligner']
+annotation = param['annotation']
+output_path = param['htseqOutPath']
+db_name = param['gsnapDbName']
+gsnap_annotation = param['gsnapAnnotation']
+
+Dict = param['symbolIDFile']
 inputpath = file_path
 
-subprocess.call('mail shl198@eng.ucsd.edu < /home/shangzhong/start.txt',shell=True)
 #=========== (0) enter the directory ================
+Message(startMessage,email)
 os.chdir(file_path)
 #=========== (1) reads files and trim ===============
-fastqFiles = list_fastq(file_path,'False')
+fastqFiles = list_files(file_path)
+if trim == 'True':
+    fastqFiles = Trimmomatic(trimmomatic,fastqFiles,phred,trimmoAdapter)
+print 'list file succeed'
 #=========== (2) run gsnap to do the mapping ========
-#map_files = gsnap(fastqFiles,db_path, db_name,gsnap_annotation,thread)
-
-starDb_path = '/opt/genome/cho/STAR_chok1Db'
-map_files = STAR(fastqFiles,starDb_path,thread)
+if aligner == 'gsnap':
+    map_files = gsnap(fastqFiles,db_path, db_name,gsnap_annotation,thread)
+else:
+    map_files = STAR(fastqFiles,db_path,thread)
+print 'align succeed'
 #=========== (3) samtools to sort the file ==========
-sorted_bam = sam2bam_sort(map_files)
+sorted_bam = sam2bam_sort(map_files,thread)
+print 'sorted succeed'
 #=========== (4) htseq_count ========================
 htseq_count(sorted_bam,annotation,file_path)
+print 'htseq count succeed'
 #=========== (5) htseq symbol to id =================
 ID_Convert(Dict,output_path,inputpath)
-
-subprocess.call('mail shl198@eng.ucsd.edu < /home/shangzhong/end.txt',shell=True)
+print 'id convert succeed'
+Message(endMessage,email)
--- DiffExpression/Diff_DEseq2.R
+++ DiffExpression/Diff_DEseq2.R
@@ -1,97 +1,61 @@
-source("http://bioconductor.org/biocLite.R")
+#!/usr/bin/env Rscript
+#source("http://bioconductor.org/biocLite.R")
 #biocLite("DESeq2")
-library("DESeq2")
-directory = '/data/shangzhong/Diff_express/Esko_project/htseqcount_geneid'
-sampleFiles <- grep("htseq_C",list.files(directory),value=TRUE)
-sampleCondition <- c(rep('WT',3),rep('test',3))
-sampleTable <- data.frame(sampleName=sampleFiles,fileName=sampleFiles,
-                          condition=sampleCondition)
-ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=
-                directory, design=~condition)
-ddsHTSeq$condition <- factor(ddsHTSeq$condition, 
-                             levels=c("WT","test"))
-dds <- DESeq(ddsHTSeq)
-res <- results(dds)
-resOrdered <- res[order(res$padj),]
-head(resOrdered)
-#plot MA
-plotMA(res, main="C4_6 VS C2_3", ylim=c(-2,2))
-#export differentially expressed genes the threshold is 0.05
-result <- resOrdered[complete.cases(resOrdered),]
-sig_result <- result[result$padj < 0.05 & (result$log2FoldChange >= 0.58 | result$log2FoldChange <=-0.58),]
-write.csv(sig_result,"C4_6VS_C1_3.csv")
-write.csv(resOrdered,'test.csv')
-#transformation dta
-rld <- rlog(dds)
-# Heat map
-library("RColorBrewer")
-library("gplots")
-#heat map of distance matrix
-distsRL <- dist(t(assay(rld)))
-mat <- as.matrix(distsRL)
-rownames(mat) <- colnames(mat) <- with(colData(dds),
-                                       paste(condition))
-colours = colorRampPalette(rev(brewer.pal(9,"Blues"))) (255)
-heatmap.2(mat, trace="none", col = colours,margin=c(10,10))
-print(plotPCA(rld, intgroup=c("condition")))
-# gene clustering
-library("genefilter")
-topVarGenes <- head(order(rowVars(assay(rld)),decreasing=TRUE),20)
-heatmap.2(assay(rld)[topVarGenes,],scale="row",trace="none",dendrogram="column",
-          col=colorRampPalette(rev(brewer.pal(9,"RdBu"))),margin=c(9,9))
+if (!require("DESeq2")) {
+  install.packages("DESeq2", dependencies = TRUE)
+  library(DESeq2)
+}
+#====== 1. set working directory and list files ========
+#filePath <- '/data/shangzhong/Diff_express/htseq_GlycoCosmic'
+args <- commandArgs(TRUE)
+filePath <- args[1]
+setwd(filePath)
+DE_file <- list.files(filePath,pattern='\\.txt$')
+compare <- read.csv("DE_pairs.csv",sep=',') # file decide which samples to compare
 
-#=====================================================================
-#============= analyze the mbmec sample ==============================
-sampleFiles <- grep("htseq_M",list.files(directory),value=TRUE)
-sampleCondition <- c(rep('creN',2),rep('creP',4),rep('Ext',4))
-sampleTable <- data.frame(sampleName=sampleFiles,fileName=sampleFiles,
-                          condition=sampleCondition)
-ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=
-                                         directory, design=~condition)
-ddsHTSeq$condition <- factor(ddsHTSeq$condition, 
-                             levels=c("Ext","creN","creP"))
-background <- assay(ddsHTSeq)
-background <- background[rowSums(background[,-1])>0,]
-background <- background[apply(background[,-1], 1, function(x) !all(x==0)),]
-write.csv(background,'background.csv')
+#====== 2. 
+col <- colnames(compare)
+row <- nrow(compare)
 
-dds <- DESeq(ddsHTSeq)
-#================== (1) creN VS creP ========================= 
-res <- results(dds, contrast = c("condition","creN","creP"))
-resOrdered <- res[order(res$padj),]
-result <- resOrdered[complete.cases(resOrdered),]
-sig_result <- result[result$padj < 0.01 & (result$log2FoldChange >= 0.7 | result$log2FoldChange <=-0.7),]
-write.csv(sig_result,"creN_VS_creP.csv")
-
-#================== (2) crepN VS Ext ========================= 
-res <- results(dds, contrast = c("condition","creN","Ext"))
-resOrdered <- res[order(res$padj),]
-result <- resOrdered[complete.cases(resOrdered),]
-sig_result <- result[result$padj < 0.01 & (result$log2FoldChange >= 1 | result$log2FoldChange <=-1),]
-write.csv(sig_result,"creN_VS_Ext.csv")
-write.csv(result,'test.csv')
-#================== (3) crepP VS Ext ========================= 
-res <- results(dds, contrast = c("condition","creP","Ext"))
-resOrdered <- res[order(res$padj),]
-result <- resOrdered[complete.cases(resOrdered),]
-sig_result <- result[result$padj < 0.01 & (result$log2FoldChange >= 0.7 | result$log2FoldChange <=-0.7),]
-write.csv(sig_result,"creP_VS_Ext.csv")
-
-#transformation
-rld <- rlog(dds)
-#heat map of distance matrix
-distsRL <- dist(t(assay(rld)))
-mat <- as.matrix(distsRL)
-rownames(mat) <- paste(rld$condition)
-colnames(mat) <- rownames(mat)
-library("gplots")
-library("RColorBrewer")
-colours = colorRampPalette(rev(brewer.pal(9,"Blues"))) (255)
-heatmap.2(mat, trace="none", col = colours,margin=c(10,10))
-print(plotPCA(rld, intgroup=c("condition")))
-
-# gene_clustering
-topVarGenes <- head(order(rowVars(assay(rld)),decreasing=TRUE),50)
-heatmap.2(assay(rld)[topVarGenes,],scale="row",trace="none",dendrogram="column",
-          col=colorRampPalette(rev(brewer.pal(9,"RdBu"))),margin=c(9,9))
-
+for (i in 1:row) {
+    # 1st part, list the control sample and test sample
+    control <- c()
+    test <- c()
+    for (j in 1:length(col)) {
+        if (grepl('control',col[j],ignore.case=TRUE) & !is.na(compare[i,j])) {
+            control <- c(control,DE_file[compare[i,j]])
+        } else if (grepl('test',col[j],ignore.case=TRUE) & !is.na(compare[i,j])) {
+            test <- c(test,DE_file[compare[i,j]])
+        }
+    }
+    
+    # 2nd part, do the DE for one compare
+    sampleFiles <- c(control,test)
+    sampleCondition <- c(rep('control',length(control)),rep('test',length(test)))
+    sampleTable <- data.frame(sampleName=sampleFiles,fileName=sampleFiles,
+                              condition=sampleCondition)
+    ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,directory=
+                                             filePath, design=~condition)
+    ddsHTSeq$condition <- factor(ddsHTSeq$condition, 
+                                 levels=c("control","test"))
+    dds <- DESeq(ddsHTSeq)
+    res <- results(dds)
+    resOrdered <- res[order(res$padj),]
+    result <- resOrdered[complete.cases(resOrdered),]
+    sig_result <- result[result$padj < 0.01 & (result$log2FoldChange >= 0.58 | result$log2FoldChange <=-0.58),]
+    outputFile <- paste('Line',toString(i),'.csv',sep="")
+    write.csv(sig_result,outputFile)
+    # output cluster figure
+    pdf(paste(strsplit(outputFile,'\\,')[[1]][1],'.pdf',sep=""))
+    rld <- rlog(dds)
+    # Heat map
+    library("RColorBrewer")
+    library("gplots")
+    #heat map of distance matrix
+    distsRL <- dist(t(assay(rld)))
+    mat <- as.matrix(distsRL)
+    rownames(mat) <- colnames(mat) <- with(colData(dds),paste(condition,sampleFiles , sep=" : "))
+    colours = colorRampPalette(rev(brewer.pal(9,"Blues"))) (255)
+    heatmap.2(mat, trace="none", col = colours,margin=c(17,17))
+    dev.off()
+}
